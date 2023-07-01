4edaaaea939d497d8c6b4a255919441d4c4ee481
nimeshRanatunge <2018cs131@stu.ucsc.cmb.ac.lk>
2023-07-01 11:51:51 +0530

    c5

--- Basic Problems/src/Main.java
+++ Basic Problems/src/Main.java
@@ -1,21 +1,5 @@
-public class Main {
-    //prime numbers between x and y
-    public static void main(String[] args) {
-        int x = 10, y= 50;
-        //11 13 17 19
-        for(int i=x;i<y;i++){
-            if(printIfPrime(i)) System.out.print(i+" ");
-        }
-    }
+import java.util.Scanner;
 
-    private static boolean printIfPrime(int n) {
-        if(n<=1) return false;
-        else{
-            for(int i=2; i<=Math.sqrt(n); i++){
-                if(n%i==0) return false;
-            }
-        }
-        return true;
-    }
+public class Main {
 
 }
