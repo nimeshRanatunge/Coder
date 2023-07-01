3401ce6017014bc2dfb008b90254885cc04de7ac
Thisara Ranatunge <2018cs131@stu.ucsc.cmb.ac.lk>
2023-07-01 10:06:39 +0530

    4

--- Basic Problems/src/Main.java
+++ Basic Problems/src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     //prime numbers between x and y
     public static void main(String[] args) {
-        System.out.println("jjj");
+        int x = 10, y= 50;
+        //11 13 17 19
+        for(int i=x;i<y;i++){
+            if(printIfPrime(i)) System.out.print(i+" ");
+        }
     }
+
+    private static boolean printIfPrime(int n) {
+        if(n<=1) return false;
+        else{
+            for(int i=2; i<=Math.sqrt(n); i++){
+                if(n%i==0) return false;
+            }
+        }
+        return true;
+    }
+
 }
