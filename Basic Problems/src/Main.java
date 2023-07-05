24eb0295d5dbe7676fe4a913f9b2804a0110975b
Thisara Ranatunge <2018cs131@stu.ucsc.cmb.ac.lk>
2023-07-05 14:51:55 +0530

    letters

--- Basic Problems/src/Main.java
+++ Basic Problems/src/Main.java
@@ -1,5 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
+//diamond
+public static void main(String[] args) {
 
+    for(int i=1; i<=6;i++){
+        for(int j=6; j>=i;j--){
+            System.out.print(" ");
+        }
+        for(int k=1; k<=i;k++){
+            System.out.print("* ");
+        }
+        System.out.print("\n");
+    }
+
+    for(int i=1; i<=5;i++){
+        for(int j=1; j<=i;j++){
+            System.out.print(" ");
+        }
+        for(int k=5; k>=i;k--){
+            System.out.print(" *");
+        }
+        System.out.print("\n");
+    }
+}
 }
